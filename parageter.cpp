b24dccb41f3eb2ba5533b07bcf7641bc7fad97f6
Ryan <Yuanzhehan@yeah.net>
2023-01-24 22:45:55 +0800

    Ryan

--- parageter.cpp
+++ parageter.cpp
@@ -5,10 +5,9 @@ int main(int argc,char * argv[])
 {
 	ofstream o;
 	o.open("List.txt",ios::app);
-    o << "NUM: " << argc << std::endl;
     for (size_t i = 0; i < argc; i++)
-		o << "PARAMETER" << i+1 << ": " << argv[i] << endl;
-	o << "-=-=-=-=-=-=-=-=-" << endl;
+		o << argv[i] << " ";
+	o << endl;
 	o.close();
     return 0;
 }
